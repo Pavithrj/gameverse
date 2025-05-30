b2ec3d0047846af516defdf688816e1207003e57
Pavithr jain <68775236+Pavithrj@users.noreply.github.com>
2025-05-30 10:22:33 +0000

    change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vi
+// https://v
 export default defineConfig({
     plugins: [react()],
     base: "/gameverse"
